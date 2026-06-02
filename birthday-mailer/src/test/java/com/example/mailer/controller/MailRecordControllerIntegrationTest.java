5dc9b3bf31fb429a1ec9256fa8c9713c9cb2f700
QuocAnh <1050080212@sv.hcmunre.edu.vn>
2026-06-02 09:00:07 +0700

    fix pom reader

--- birthday-mailer/src/test/java/com/example/mailer/controller/MailRecordControllerIntegrationTest.java
+++ birthday-mailer/src/test/java/com/example/mailer/controller/MailRecordControllerIntegrationTest.java
@@ -7,8 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.mock;
 
 import java.time.LocalDateTime;
 
@@ -20,8 +25,20 @@ import static org.hamcrest.Matchers.hasSize;
 @SpringBootTest
 @AutoConfigureMockMvc
 @org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase(replace = org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.ANY)
-@org.springframework.test.context.TestPropertySource(properties = "spring.jpa.hibernate.ddl-auto=create-drop")
+@org.springframework.test.context.TestPropertySource(properties = {
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.rabbitmq.host=localhost",
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration"
+})
 public class MailRecordControllerIntegrationTest {
+
+    @TestConfiguration
+    static class MailSenderMockConfig {
+        @Bean
+        public JavaMailSender javaMailSender() {
+            return mock(JavaMailSender.class);
+        }
+    }
 
     @Autowired
     MockMvc mvc;
