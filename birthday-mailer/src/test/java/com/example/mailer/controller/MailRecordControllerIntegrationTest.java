5b634c3cec8e7c494b3a02b90bf2b69ed886cc97
QuocAnh <1050080212@sv.hcmunre.edu.vn>
2026-06-02 09:34:26 +0700

    fix controller test

--- birthday-mailer/src/test/java/com/example/mailer/controller/MailRecordControllerIntegrationTest.java
+++ birthday-mailer/src/test/java/com/example/mailer/controller/MailRecordControllerIntegrationTest.java
@@ -5,15 +5,15 @@ import com.example.mailer.repository.MailRecordRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.Mockito.mock;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 import java.time.LocalDateTime;
 
@@ -24,27 +24,24 @@ import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase(replace = org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.ANY)
-@org.springframework.test.context.TestPropertySource(properties = {
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@TestPropertySource(properties = {
         "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.rabbitmq.host=localhost",
-        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration"
+        "spring.rabbitmq.listener.simple.auto-startup=false"
 })
 public class MailRecordControllerIntegrationTest {
 
-    @TestConfiguration
-    static class MailSenderMockConfig {
-        @Bean
-        public JavaMailSender javaMailSender() {
-            return mock(JavaMailSender.class);
-        }
-    }
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @MockBean
+    private ConnectionFactory connectionFactory;
 
     @Autowired
-    MockMvc mvc;
+    private MockMvc mvc;
 
     @Autowired
-    MailRecordRepository repo;
+    private MailRecordRepository repo;
 
     @BeforeEach
     void setup() {
